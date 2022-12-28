46ba1d34cdb79099c513f7b4744ac0d85d445d7f
Kevin <kevinbryanbuenaseda@gmail.com>
2022-12-28 08:30:23 +0800

    update reviews

--- src/views/Reviews.js
+++ src/views/Reviews.js
@@ -5,7 +5,7 @@ import { Outlet, Link } from "react-router-dom";
 import { AiOutlineRight } from "react-icons/ai";
 
 function Reviews() {
-  const [review, setReview] = useState([
+  const [reviewSafety, setReviewSafety] = useState([
     {
       message:
         "Guy Finelli as The Safety, is an emulation to those who believe in the miracles of second chances.",
@@ -67,6 +67,86 @@ function Reviews() {
       creator: "Jill Steier",
     },
   ]);
+  const [reviewMissPretty, setReviewMissPretty] = useState([
+    {
+      message: "A thoughtful book that was fun to read",
+      creator: "Christian Boesen",
+    },
+    {
+      message: "A story with a futuristic touch.",
+      creator: "	Shobana Gomes",
+    },
+    {
+      message: "P. E. Fischetti’s Best Novel To Date",
+      creator: "Amazon Customer",
+    },
+  ]);
+  const [reviewHotInAugust, setReviewHotInAugust] = useState([
+    {
+      message:
+        "A Stimulating, Immersive Read! It's about ordinary lives and a love for baseball.",
+      creator: "Shobana Gomes",
+    },
+    {
+      message: "Trilogy! Trilogy! Trilogy!",
+      creator: "Annie",
+    },
+    {
+      message: "Big Train's Backyard is a winner",
+      creator: "Edmund Matthew S",
+    },
+    {
+      message: "A thoughtful book that was fun to read",
+      creator: "Richard Murphy",
+    },
+    {
+      message: "Big Train's Back Yard",
+      creator: "MarianPhilly",
+    },
+    {
+      message: "Fischetti Writes A Winner!",
+      creator: "Art in RI",
+    },
+    {
+      message:
+        "If you enjoy the varieties of family relationships, baseball, history, and a little statistics, this book is for you!!",
+      creator: "Bill Giuliani",
+    },
+    {
+      message: "Fun book with a baseball setting.",
+      creator: "Jill Steier",
+    },
+    {
+      message: "Good read for baseball lovers",
+      creator: "PMH",
+    },
+    {
+      message: "I was very into this book and looked forward each ...",
+      creator: "Ed Danberry",
+    },
+    {
+      message: "Must Read for DC Baseball Fans",
+      creator: "JN",
+    },
+    {
+      message: "Big Train Backyard",
+      creator: "Amazon Customer",
+    },
+    {
+      message: "A can't put it down read",
+      creator: "Christian Boesen",
+    },
+    {
+      message:
+        "When I saw the title and Iconic baseball player taking his power stroke full cut,I knew it was about my childhood sport Dad taught me to love.",
+      creator: "RMIII",
+    },
+    {
+      message:
+        "Big Train's backyard - refreshing novel with an easy to read story line",
+      creator: "Stacy Ciano",
+    },
+  ]);
   return (
     <>
       <div className="h-full bg-black relative flex md:mt-[88px] mt-[82px] ">
@@ -100,6 +180,61 @@ function Reviews() {
                     <AiOutlineRight />
                   </IconContext.Provider>
                 </Link>
+                <div>
+                  <div className="text-5xl font-bold text-white">Amazon</div>
+                  <div className="text-4xl font-bold py-6 text-white text-center">
+                    “The Safety”
+                  </div>
+                  <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
+                    {reviewSafety.map((item, index) => {
+                      return (
+                        <div
+                          className={`${
+                            index % 2 === 0 ? "border-r-2" : "border-l-2"
+                          } border-b-2 p-6 italic`}
+                          key={index}
+                        >
+                          <div className="flex">
+                            {" "}
+                            <IconContext.Provider
+                              value={{ color: "#FF9900", size: "1.5em" }}
+                            >
+                              <AiFillStar />
+                            </IconContext.Provider>
+                            <IconContext.Provider
+                              value={{ color: "#FF9900", size: "1.5em" }}
+                            >
+                              <AiFillStar />
+                            </IconContext.Provider>
+                            <IconContext.Provider
+                              value={{ color: "#FF9900", size: "1.5em" }}
+                            >
+                              <AiFillStar />
+                            </IconContext.Provider>
+                            <IconContext.Provider
+                              value={{ color: "#FF9900", size: "1.5em" }}
+                            >
+                              <AiFillStar />
+                            </IconContext.Provider>
+                            <IconContext.Provider
+                              value={{ color: "#FF9900", size: "1.5em" }}
+                            >
+                              <AiFillStar />
+                            </IconContext.Provider>
+                          </div>
+
+                          <div className="text-gray-300 ">"{item.message}"</div>
+                          <div className="font-semibold">
+                            -{item.creator}{" "}
+                            <span className="text-[#FF9900]">
+                              Amazon Reviews
+                            </span>{" "}
+                          </div>
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
               </div>
               <div className="">
                 <div className="py-8 uppercase text-white text-3xl font-semibold">
@@ -136,10 +271,63 @@ function Reviews() {
           <div>
             <div className="text-5xl font-bold text-white">Amazon</div>
             <div className="text-4xl font-bold py-6 text-white text-center">
-              “The Safety”
+              “Miss Pretty Please”
             </div>
             <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
-              {review.map((item, index) => {
+              {reviewMissPretty.map((item, index) => {
+                return (
+                  <div
+                    className={`${
+                      index % 2 === 0 ? "border-r-2" : "border-l-2"
+                    } border-b-2 p-6 italic`}
+                    key={index}
+                  >
+                    <div className="flex">
+                      {" "}
+                      <IconContext.Provider
+                        value={{ color: "#FF9900", size: "1.5em" }}
+                      >
+                        <AiFillStar />
+                      </IconContext.Provider>
+                      <IconContext.Provider
+                        value={{ color: "#FF9900", size: "1.5em" }}
+                      >
+                        <AiFillStar />
+                      </IconContext.Provider>
+                      <IconContext.Provider
+                        value={{ color: "#FF9900", size: "1.5em" }}
+                      >
+                        <AiFillStar />
+                      </IconContext.Provider>
+                      <IconContext.Provider
+                        value={{ color: "#FF9900", size: "1.5em" }}
+                      >
+                        <AiFillStar />
+                      </IconContext.Provider>
+                      <IconContext.Provider
+                        value={{ color: "#FF9900", size: "1.5em" }}
+                      >
+                        <AiFillStar />
+                      </IconContext.Provider>
+                    </div>
+
+                    <div className="text-gray-300 ">"{item.message}"</div>
+                    <div className="font-semibold">
+                      -{item.creator}{" "}
+                      <span className="text-[#FF9900]">Amazon Reviews</span>{" "}
+                    </div>
+                  </div>
+                );
+              })}
+            </div>
+          </div>
+          <div>
+            <div className="text-5xl font-bold text-white">Amazon</div>
+            <div className="text-4xl font-bold py-6 text-white text-center">
+              “Hot in August”
+            </div>
+            <div className="grid md:grid-cols-2 grid-cols-1 gap-6">
+              {reviewHotInAugust.map((item, index) => {
                 return (
                   <div
                     className={`${
